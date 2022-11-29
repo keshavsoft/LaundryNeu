953805cc5721c9061f65eefc290827cbc3226425
keshavsoft <nknnkumar@yahoo.com>
2022-11-29 12:51:53 +0530

    keshav

--- resources/Laundry/Dal/Transactions/Bookings/FuncsForPk/Start.js
+++ resources/Laundry/Dal/Transactions/Bookings/FuncsForPk/Start.js
@@ -4,14 +4,22 @@ let StartFunc = async () => {
     let LocalReturnObject = { KTF: false, KResult: "" };
 
     try {
-        let LocalJsonFileName = "Bookings.json";
+        //let LocalJsonFileName = "Bookings.json";
 
         // let LocalCustomersData = await Neutralino.filesystem.readFile(`./KData/JSON/2017/Data/Transactions/${LocalJsonFileName}`);
         // console.log("LocalCustomersData : ", LocalCustomersData);
         // let LocalCustomersDataAsJson = JSON.parse(LocalCustomersData);
 
-        let LocalCustomersDataAsJson = await PullFuncsStartFunc();
-        let LocalKeys = Object.keys(LocalCustomersDataAsJson);
+        let LocalFromPull = await PullFuncsStartFunc();
+
+        if (LocalFromPull.KTF === false) {
+            LocalReturnObject.KReason = LocalFromPull.KReason;
+            return await LocalReturnObject;
+        };
+
+        //console.log("LocalCustomersDataAsJson ---------- : ", LocalFromPull.JsonData);
+        
+        let LocalKeys = Object.keys(LocalFromPull.JsonData);
         let LocalLastKey = LocalKeys[LocalKeys.length - 1];
 
         LocalReturnObject.KTF = true;
